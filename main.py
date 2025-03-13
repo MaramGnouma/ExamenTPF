aac4e66607c021aa692771f8beca212ba7ea7e82
Maram Gnouma <maramgnouma200@gmail.com>
2025-03-13 23:32:21 +0100

    Modifier 22 de main.py ds feature

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 
-def somme (L) :
-S=0
-for e in L:
-s+=e
-return s
-my_list=[1,5,7]
-print ('la somme est', sum(my_list) )
+if my_list:
+print ('la somme est: ', sum (my_list) )
+print ('le max est:', max(my_list) )
+print ('le min est:',min(my_list))
+else:
+print ('liste vide')
